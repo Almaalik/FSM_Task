eccaa8fa550098dbddd66332353b846616fbd11c
Albdul Maalik <abdul.m@vajro.com>
2026-02-13 19:26:12 +0530

    Initial Commit

---
+++ FSM/FSMApp.swift
@@ -0,0 +1,17 @@
+//
+//  FSMApp.swift
+//  FSM
+//
+//  Created by AlMaalik's Mac on 13/02/26.
+//
+
+import SwiftUI
+
+@main
+struct FSMApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
